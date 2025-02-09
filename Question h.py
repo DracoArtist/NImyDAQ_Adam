23e79a27f485c994c01d686ca5ba9e5acb0c7aa2
Adam Zolnai <adam.zolnai@icloud.com>
2025-02-09 13:56:37 -0500

    homework in progress

--- Question h.py
+++ Question h.py
@@ -1,5 +1,6 @@
 import json
 from my_classes.statistics_file import Statistics
+from sympy import symbols, diff
 
 """
 Calcul l'incertitude pour la question h
@@ -14,14 +15,24 @@ data = content['data']
 
 mean, std = stat.compute_mean_and_deviation(data)
 
-alpha_V2 = ((std / (len(data)**0.5))**2 + 0.0001**2)**0.5
-alpha_V1 = 0.009
-V1 = 2
+R1 = symbols('R1')
+V2 = symbols('V2')
+Vt = symbols('Vt')
+u_R1 = symbols('u_R1')
+u_V2 = symbols('u_V2')
+u_Vt = symbols('u_Vt')
 
-relative_R1 = 0.05
+R2 = R1*V2 / (Vt-V2)
 
-R2 = 10801.332
+u_R2 = ( (diff(R2, R1)*u_R1)**2 +  (diff(R2, V2)*u_V2)**2 + (diff(R2, Vt)*u_Vt)**2)**0.5
 
-uncertainty = R2*(relative_R1**2 + (alpha_V2 / mean)**2 + ((alpha_V2**2 + alpha_V1**2)**0.5 / (2-mean))**2)**0.5
-
-print(f'{uncertainty: .12f}')
+print(
+    u_R2.evalf(subs={
+        R1:100,  # R1 c'est la résistance de 100 ohm
+        V2:mean,  # V2 c'est la tension mesuré
+        Vt:2,  # Vt c'est la tension totale de 2V
+        u_R1:5,  # l'incertitude sur R1 c'est 0.05*100 = 5 ohm
+        u_V2:0.00009369,  # On néglige l'incertitude de type A
+        u_Vt:0.009  # Incertitude donnée par le fabriquant
+    })
+)
